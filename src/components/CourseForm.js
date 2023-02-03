1808a1a48c84f869392d3f6fa115f0c8721071cd
Sherry He <xsherryhe@gmail.com>
2023-02-03 15:52:59 -0500

    Extracted resource form into separate component

--- src/components/CourseForm.js
+++ src/components/CourseForm.js
@@ -1,86 +1,18 @@
-import { useContext, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import fetcher from '../fetcher';
-import { capitalize } from '../utilities';
+import ResourceForm from './ResourceForm';
 
-import MessageContext from './contexts/MessageContext';
-import Field from './Field';
-import withFormValidation from './higher-order/withFormValidation';
+export default function CourseForm(props) {
+  const fields = [
+    { attribute: 'title', required: true },
+    { attribute: 'description', type: 'textarea', required: true },
+    {
+      attribute: 'instructor_logins',
+      defaultValue: (defaultValues) =>
+        defaultValues.instructors?.map(({ username }) => username)?.join(', '),
+      type: 'textarea',
+      labelText: 'Instructors (username or email, comma-separated)',
+      attributeText: 'Instructors',
+    },
+  ];
 
-function CourseFormBase({
-  validate,
-  toValidate,
-  errors,
-  handleErrors,
-  action,
-  defaultValues = {},
-  id,
-  completeAction,
-}) {
-  const preAction = { create: 'new', update: 'edit' }[action];
-  const [loading, setLoading] = useState(false);
-  const navigate = useNavigate();
-  const setMessage = useContext(MessageContext).set;
-
-  function completeCourseAction(data) {
-    setMessage(`Successfully ${action}d course.`);
-    if (completeAction) completeAction(data);
-    else navigate(`/course/${id || data.id}`, { state: { courseData: data } });
-  }
-
-  async function handleSubmit(e) {
-    e.preventDefault();
-    if (!validate(e.target)) return;
-
-    setLoading(true);
-    const response = await fetcher(`courses${id ? `/${id}` : ''}`, {
-      method: { create: 'POST', update: 'PATCH' }[action],
-      body: new FormData(e.target),
-    });
-    const data = await response.json();
-    if (response.status < 400) completeCourseAction(data);
-    else handleErrors(data);
-    setLoading(false);
-  }
-
-  return (
-    <form noValidate onSubmit={handleSubmit}>
-      <h1>{capitalize(preAction)} Course</h1>
-      <Field
-        prefix="course"
-        attributes={['title']}
-        defaultValue={defaultValues.title}
-        errors={errors}
-        toValidate={toValidate}
-        required={true}
-      />
-      <Field
-        prefix="course"
-        attributes={['description']}
-        defaultValue={defaultValues.description}
-        type="textarea"
-        errors={errors}
-        toValidate={toValidate}
-        required={true}
-      />
-      <Field
-        prefix="course"
-        attributes={['instructor_logins']}
-        defaultValue={defaultValues.instructors
-          ?.map(({ username }) => username)
-          ?.join(', ')}
-        type="textarea"
-        labelText="Instructors (username or email, comma-separated)"
-        attributeText="Instructors"
-        errors={errors}
-        toValidate={toValidate}
-      />
-      <button disabled={loading} type="submit">
-        {capitalize(action)} Course
-      </button>
-    </form>
-  );
+  return <ResourceForm resource="course" fields={fields} {...props} />;
 }
-
-const CourseForm = withFormValidation(CourseFormBase, { authenticated: true });
-export default CourseForm;
---
+++ src/components/ResourceForm.js
@@ -0,0 +1,77 @@
+import { useContext, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import fetcher from '../fetcher';
+import { capitalize } from '../utilities';
+
+import MessageContext from './contexts/MessageContext';
+import Field from './Field';
+import withFormValidation from './higher-order/withFormValidation';
+
+function ResourceFormBase({
+  resource,
+  fields,
+  defaultValues = {},
+  action,
+  validate,
+  toValidate,
+  errors,
+  handleErrors,
+  id,
+  completeAction,
+}) {
+  const preAction = { create: 'new', update: 'edit' }[action];
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  const setMessage = useContext(MessageContext).set;
+
+  function completeFormAction(data) {
+    setMessage(`Successfully ${action}d ${resource}.`);
+    if (completeAction) completeAction(data);
+    else
+      navigate(`/${resource}/${id || data.id}`, {
+        state: { [`${resource}Data`]: data },
+      });
+  }
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    if (!validate(e.target)) return;
+
+    setLoading(true);
+    const response = await fetcher(`${resource}s${id ? `/${id}` : ''}`, {
+      method: { create: 'POST', update: 'PATCH' }[action],
+      body: new FormData(e.target),
+    });
+    const data = await response.json();
+    if (response.status < 400) completeFormAction(data);
+    else handleErrors(data);
+    setLoading(false);
+  }
+
+  return (
+    <form noValidate onSubmit={handleSubmit}>
+      <h1>
+        {capitalize(preAction)} {capitalize(resource)}
+      </h1>
+      {fields.map(({ attribute, type, labelText, attributeText, required }) => (
+        <Field
+          prefix={resource}
+          attributes={[attribute]}
+          type={type}
+          labelText={labelText}
+          attributeText={attributeText}
+          defaultValue={defaultValues[attribute]}
+          errors={errors}
+          toValidate={toValidate}
+          required={required}
+        />
+      ))}
+      <button disabled={loading} type="submit">
+        {capitalize(action)} {capitalize(resource)}
+      </button>
+    </form>
+  );
+}
+
+const ResourceForm = withFormValidation(ResourceFormBase);
+export default ResourceForm;

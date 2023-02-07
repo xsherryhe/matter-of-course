87e7fe809e8445c764d7d888b34c85593c2be137
Sherry He <xsherryhe@gmail.com>
2023-02-07 12:22:14 -0500

    Added UI for directly destroying a lesson

--- src/components/Lesson.js
+++ src/components/Lesson.js
@@ -1,16 +1,38 @@
+import { Link, useParams } from 'react-router-dom';
 import asResource from './higher-order/asResource';
 import LessonForm from './LessonForm';
 
 function LessonBase({
   resource: lesson,
-  setResource: setLesson,
+  error,
   editForm,
   editButton,
   deleteButton,
 }) {
+  const { courseId } = useParams();
+
+  if (error) {
+    let displayError = '';
+    if (error.data?.error)
+      displayError = <div className="error">{error.data.error}</div>;
+    if (typeof error === 'string')
+      displayError = <div className="error">{error}</div>;
+    return (
+      <div>
+        <Link to={`/course/${courseId}`}>Back to Course</Link>
+        {displayError}
+      </div>
+    );
+  }
+
   let main = (
     <main>
-      {editButton}
+      {lesson.authorized && (
+        <div className="buttons">
+          {editButton}
+          {deleteButton}
+        </div>
+      )}
       {lesson.lesson_sections.map(({ id, title, body }) => (
         <div key={id} className="section">
           <h2>{title}</h2>
@@ -21,6 +43,7 @@ function LessonBase({
   );
   return (
     <div>
+      <Link to={`/course/${courseId}`}>Back to Course</Link>
       <h1>{lesson.title}</h1>
       {editForm || main}
     </div>
@@ -29,5 +52,6 @@ function LessonBase({
 
 const Lesson = asResource(LessonBase, LessonForm, 'lesson', {
   formHeading: false,
+  catchError: false,
 });
 export default Lesson;

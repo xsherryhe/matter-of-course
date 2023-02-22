3cf19964e6e749b48a4f4bf55494a678ea40541d
Sherry He <xsherryhe@gmail.com>
2023-02-22 01:03:24 -0500

    Added pagination to invitations component

--- src/components/Invitations.js
+++ src/components/Invitations.js
@@ -1,35 +1,56 @@
 import { useEffect, useState } from 'react';
 import fetcher from '../fetcher';
+import withPagination from './higher-order/withPagination';
 import Invitation from './Invitation';
 
-export default function Invitations() {
+function InvitationsBase({
+  invitationsPage,
+  updateInvitationsPage,
+  invitationsPagination,
+}) {
   const [invitations, setInvitations] = useState(null);
+  const [error, setError] = useState(null);
+
+  function handleErrors({ data }) {
+    if (data.error) setError(data.error);
+  }
 
   useEffect(() => {
     async function getInvitations() {
-      const response = await fetcher('instruction_invitations');
-      setInvitations(response.data);
+      const response = await fetcher('instruction_invitations', {
+        query: `page=${invitationsPage}`,
+      });
+      if (response.status < 400) {
+        setInvitations(response.data.invitations);
+        updateInvitationsPage(response.data);
+      } else handleErrors(response);
     }
 
     async function updateInvitations() {
       await fetcher('instruction_invitations', {
         method: 'PATCH',
-        query: 'read=true',
+        query: `read=true&page=${invitationsPage}`,
       });
     }
 
     getInvitations();
     return updateInvitations;
-  }, []);
+  }, [invitationsPage, updateInvitationsPage]);
 
-  if (!invitations) return 'Loading...';
-
-  let main;
-  if (invitations.length)
-    main = invitations.map((invitation) => (
-      <Invitation key={invitation.id} invitation={invitation} />
-    ));
-  else main = 'No invitations yet!';
+  let main = 'Loading...';
+  if (error) main = <div className="error">{error}</div>;
+  else if (invitations) {
+    if (invitations.length)
+      main = (
+        <div>
+          {invitations.map((invitation) => (
+            <Invitation key={invitation.id} invitation={invitation} />
+          ))}
+          {invitationsPagination}
+        </div>
+      );
+    else main = 'No invitations yet!';
+  }
 
   return (
     <div>
@@ -38,3 +59,6 @@ export default function Invitations() {
     </div>
   );
 }
+
+const Invitations = withPagination(InvitationsBase, 'invitations');
+export default Invitations;

ab18e22692b5d58d56995f76abadd8df76a01c77
Sherry He <xsherryhe@gmail.com>
2023-03-26 21:51:40 -0400

    Fix spacing issues on List component

--- src/components/List.js
+++ src/components/List.js
@@ -1,9 +1,33 @@
+import '../styles/List.css';
+
 export default function List({ items }) {
-  return items.map((element, i) => {
-    if (i === items.length - 1 && i > 0)
-      return <span key={i}>and {element}</span>;
-    if (items.length >= 3) return <span key={i}>{element}, </span>;
-    if (items.length > 1) return <span key={i}>{element} </span>;
-    return <span key={i}>{element}</span>;
-  });
+  if (items.length === 0) return;
+  if (items.length === 1) return items[0];
+  if (items.length === 2)
+    return (
+      <span className="list">
+        {items[0]}
+        <span className="list-and"> and </span>
+        {items[1]}
+      </span>
+    );
+
+  const spans = items.reduce((arr, element, i) => {
+    const newElements = [element];
+    if (i < items.length - 1)
+      newElements.push(
+        <span className="list-comma">
+          , {i === items.length - 2 ? 'and ' : ''}
+        </span>
+      );
+    return [...arr, ...newElements];
+  }, []);
+
+  return (
+    <span className="list">
+      {spans.map((span, i) => (
+        <span key={i}>{span}</span>
+      ))}
+    </span>
+  );
 }
